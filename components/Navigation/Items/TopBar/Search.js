7fbc8af1686558785ea162966db5a503bb50df1f
Ethan-Hill <ethanhilljay@outlook.com>
2021-03-02 10:02:02 +0000

    Update Search.js

--- components/Navigation/Items/TopBar/Search.js
+++ components/Navigation/Items/TopBar/Search.js
@@ -1,4 +1,8 @@
+import { useSession } from "next-auth/client"
+
 export default function Search({ handleSearch, handleClear }) {
+  const [session] = useSession()
+
   const handlePress = (e) => {
     console.log(e.key === "Enter")
     if (e.key === "Enter") {
@@ -9,28 +13,32 @@ export default function Search({ handleSearch, handleClear }) {
     const input = document.querySelector(".input")
     input.value = ""
   }
-  return (
-    <div className="flex flex-col items-center justify-center flex-auto sm:justify-center dark:text-white">
-      <div className="flex flex-col">
-        <sl-input
-          placeholder="Search"
-          size="large"
-          onKeyPress={handlePress}
-          tabIndex="0"
-          class="input"
-        >
-          <sl-icon name="search" slot="prefix"></sl-icon>
-          <sl-icon
-            name="x"
-            slot="suffix"
-            style={{ cursor: "pointer" }}
-            onClick={() => {
-              handleClear()
-              clear()
-            }}
-          ></sl-icon>
-        </sl-input>
+  if (session) {
+    return (
+      <div className="flex flex-col items-center justify-center flex-auto sm:justify-center dark:text-white">
+        <div className="flex flex-col">
+          <sl-input
+            placeholder="Search"
+            size="large"
+            onKeyPress={handlePress}
+            tabIndex="0"
+            class="input"
+          >
+            <sl-icon name="search" slot="prefix"></sl-icon>
+            <sl-icon
+              name="x"
+              slot="suffix"
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                handleClear()
+                clear()
+              }}
+            ></sl-icon>
+          </sl-input>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return null
+  }
 }
